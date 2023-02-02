8dd50197f3030ffda59615a5e61c529795661d38
khushwant singh <khushwantrathore7865@gmail.com>
2023-02-02 11:58:05 +0530

    2nd commit

--- doctor/views.py
+++ doctor/views.py
@@ -3,6 +3,11 @@ from .models import Doctorrequest, doctor
 
 
 # Create your views here.
+def displayrequest(request):
+    user = request.user
+    d = doctor.objects.get(username=user)
+    doc = Doctorrequest.objects.filter(department=d.department)
+
 def acceptrequest(request, pk):
     doc = doctor.objects.get(username=request.user)
     d = Doctorrequest.objects.get(pk=pk)
